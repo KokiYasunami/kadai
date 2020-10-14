15e24f076a349799500ce15850de45f771c8ada9
KokiYasunami <ndc15464@gmail.com>
2020-10-14 05:36:29 +0000

    課題２を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,16 @@
+const firstName = 'koki'
+const familyName = 'yasunami'
+
+console.log(firstName + familyName)
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog = false
+const cat = true
+//犬または猫を飼っている
+console.log(dog || cat)
+//犬と猫両方飼っている
+console.log(dog && cat)
+//犬も猫も飼っていない
+console.log(!dog && !cat)
+console.log(!(dog || cat))
